07999b3ebcbfbc3ef38a83fbb7a17bd1a4a5313b
alexsan-dev <alexdsantosv@gmail.com>
2021-07-11 01:11:47 -0600

    [ Add ] ssr function

--- providers/theme.ts
+++ providers/theme.ts
@@ -1,40 +1,41 @@
-import { createTheme } from "@material-ui/core/styles";
+import { createTheme } from '@material-ui/core/styles'
 
 export const theme = createTheme({
-  typography: {
-    button: {
-      textTransform: "none"
-    },
-    fontFamily: "Orkney"
-  },
-  overrides: {
-    MuiPaper: {
-      rounded: {
-        borderRadius: "10px"
-      },
-      root: {
-        borderRadius: "10px",
-        fontFamily: "Orkney"
-      }
-    },
-    MuiPickersModal: {
-      dialogRootWider: {
-        minWidth: "340px"
-      }
-    },
-    // @ts-ignore
-    MuiPickersBasePicker: {
-      pickerView: {
-        minWidth: "340px"
-      }
-    }
-  },
-  palette: {
-    primary: {
-      main: "#511F73"
-    },
-    info: {
-      main: "#346898"
-    }
-  }
-});
+	typography: {
+		button: {
+			textTransform: 'none',
+		},
+		fontFamily: 'Orkney',
+	},
+	overrides: {
+		MuiPaper: {
+			rounded: {
+				borderRadius: '10px',
+			},
+			root: {
+				borderRadius: '10px',
+				fontFamily: 'Orkney',
+			},
+		},
+		// @ts-ignore
+		MuiPickersModal: {
+			dialogRootWider: {
+				minWidth: '340px',
+			},
+		},
+		// @ts-ignore
+		MuiPickersBasePicker: {
+			pickerView: {
+				minWidth: '340px',
+			},
+		},
+	},
+	palette: {
+		primary: {
+			main: '#511F73',
+		},
+		info: {
+			main: '#346898',
+		},
+	},
+})
