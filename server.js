6766865004593d342c7c11d265fd2e947377f2ef
alexsan-dev <alexdsantosv@gmail.com>
2021-07-21 17:49:47 -0600

    [ Fix ] server

--- server.js
+++ server.js
@@ -1,27 +1,19 @@
-// FIREBASE
-const { https } = require('firebase-functions')
+// IMPORTS
+const { https, logger } = require('firebase-functions')
 const { default: next } = require('next')
 
-// DEV OR PROD
-const isDev = process.env.NODE_ENV !== 'production'
-
-// NEXT CONFIG
-const server = next({
-	dev: isDev,
-	conf: { distDir: `${path.relative(process.cwd(), __dirname)}/.next` },
+// NEXT
+const nextjsServer = next({
+	dev: false,
+	conf: { distDir: '.next' },
 })
 
-// FUNCTION
-const nextjsHandle = server.getRequestHandler()
+const nextjsHandle = nextjsServer.getRequestHandler()
+
+// CLOUD FUNC
 exports.nextServer = https.onRequest((req, res) => {
-	return server
-		.prepare()
-		.then(() =>
-			nextjsHandle(req, res).catch((err) => {
-				throw new Error(err)
-			})
-		)
-		.catch((err) => {
-			throw new Error(err)
-		})
+	return nextjsServer.prepare().then(() => {
+		logger.info(req.path, req.query)
+		return nextjsHandle(req, res)
+	})
 })
