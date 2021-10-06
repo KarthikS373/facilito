414b8644b42704e8164ad02111dc860e41aa2041
Alex Santos <alexdsantosv@gmail.com>
2021-10-06 11:49:05 -0600

    [ Add ] auth required to apis

--- pages/api/logout.ts
+++ pages/api/logout.ts
@@ -1,5 +1,6 @@
 // TIPOS
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { authRequired } from 'router/tools'
 
 // DATOS
 type RespData = { success: boolean; error?: string }
@@ -10,19 +11,30 @@ type RespData = { success: boolean; error?: string }
  * @param {NextApiRequest} req
  * @param {NextApiResponse<RespData>} res
  */
-const handler = async (req: NextApiRequest, res: NextApiResponse<RespData>): Promise<void> => {
-	// PARAMS
-	const { method } = req
+const handler = async (req: NextApiRequest, res: NextApiResponse<RespData>): Promise<void> =>
+	authRequired(
+		req,
+		async () => {
+			// PARAMS
+			const { method } = req
 
-	switch (method) {
-		case 'POST':
-			res.setHeader('Set-Cookie', `__session=deleted;Path=/;expires=Thu, 01 Jan 1970 00:00:00 GMT`)
-			res.status(200).json({ success: true })
-			break
-		default:
-			res.setHeader('Allow', ['POST'])
-			res.status(405).json({ success: false, error: `Method ${method} Not Allowed` })
-	}
-}
+			// METHODS
+			switch (method) {
+				case 'POST':
+					res.setHeader(
+						'Set-Cookie',
+						`__session=deleted;Path=/;expires=Thu, 01 Jan 1970 00:00:00 GMT`
+					)
+					res.status(200).json({ success: true })
+					break
+				default:
+					res.setHeader('Allow', ['POST'])
+					res.status(405).json({ success: false, error: `Method ${method} Not Allowed` })
+			}
+		},
+		async () => {
+			res.status(401).json({ success: false, error: 'Unauthorized Request' })
+		}
+	)
 
 export default handler
--- router/tools.ts
+++ router/tools.ts
@@ -1,8 +1,34 @@
 // TIPOS
-import type { GetServerSideProps } from 'next'
+import type { GetServerSideProps, NextApiRequest } from 'next'
 
 // FIREBASE Y NOOKIES
 import { firebaseAdmin } from 'keys/firebase-admin'
+
+/**
+ * Auth obligatorio
+ * @description Verifica un endpoint con Auth
+ * @param {GetServerSidePropsContext<never>} ctx
+ * @param {EmptyFunction} callback
+ * @param {EmptyFunction} onError
+ */
+export const authRequired = async <T>(
+	req: NextApiRequest,
+	callback: () => Promise<T>,
+	onError?: () => Promise<T>
+): Promise<T> => {
+	// COOKIE
+	const sessionCookie = req.cookies.__session || ''
+
+	// VERIFICAR TOKEN
+	let idToken: firebaseAdmin.auth.DecodedIdToken | null = null
+	if (sessionCookie?.length)
+		idToken = await firebaseAdmin.auth().verifySessionCookie(sessionCookie, true)
+
+	// EVENTO
+	if (idToken) return await callback()
+	else if (onError) return await onError()
+	else return {} as T
+}
 
 /**
  * Ruta protegida
@@ -10,45 +36,30 @@ import { firebaseAdmin } from 'keys/firebase-admin'
  * @param ctx
  */
 const isProtectedRoute: GetServerSideProps = async (ctx) => {
-	if (process.env.NODE_ENV === 'production') {
-		// COOKIE
-		const path: string = ctx.resolvedUrl
-		const sessionCookie = ctx.req.cookies.__session || ''
-
-		// VERIFICAR TOKEN
-		let idToken: firebaseAdmin.auth.DecodedIdToken | null = null
-		if (sessionCookie?.length)
-			idToken = await firebaseAdmin.auth().verifySessionCookie(sessionCookie, true)
-
-		// REDIRECT
-		if (idToken) {
-			return {
-				redirect:
-					path === '/cuenta'
-						? {
-								permanent: false,
-								destination: '/formularios',
-						  }
-						: undefined,
-				props: {} as never,
-			}
-		} else {
-			// REDIRECT A CUENTA SI NO HAY USUARIO
-			return {
-				redirect:
-					path !== '/cuenta'
-						? {
-								permanent: false,
-								destination: '/cuenta',
-						  }
-						: undefined,
-				props: {} as never,
-			}
-		}
-	} else
-		return {
+	const path: string = ctx.resolvedUrl
+	return await authRequired(
+		ctx.req as NextApiRequest,
+		async () => ({
+			redirect:
+				path === '/cuenta'
+					? {
+							permanent: false,
+							destination: '/formularios',
+					  }
+					: undefined,
 			props: {} as never,
-		}
+		}),
+		async () => ({
+			redirect:
+				path !== '/cuenta'
+					? {
+							permanent: false,
+							destination: '/cuenta',
+					  }
+					: undefined,
+			props: {} as never,
+		})
+	)
 }
 
 export default isProtectedRoute
