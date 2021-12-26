6fd73a99b8229af8145c1eb980d9e5d293a126dd
Alex Santos <alexdsantosv@gmail.com>
2021-12-26 13:17:11 -0600

    [ Add ] permissions provider

--- providers/permissions.tsx
+++ providers/permissions.tsx
@@ -1,8 +1,32 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 // REACT
 import React, { useEffect, useContext } from 'react'
 import BusinessContext from 'context/business'
 import { useRouter } from 'next/router'
 import ROUTES from 'router/routes'
+
+// FUNCION
+const lockFunction = (
+	path: string,
+	callBack: () => Promise<boolean>,
+	permissions?: CompanyPermissions
+) => {
+	if (permissions)
+		if (
+			((path.match(/\/f\/.+$/) || path.match(/\/f\/.+\/respuestas$/)) && !permissions?.forms) ||
+			(path === ROUTES.calendar && !permissions?.calendar) ||
+			((path === ROUTES.products || path.startsWith('/p/')) && !permissions?.products) ||
+			((path === ROUTES.tracking || path.match(/\/t\/.+\/editar$/)) && !permissions?.tracking)
+		) {
+			callBack().then(() => {
+				window.Alert({
+					title: 'Ocurrió un error',
+					body: 'Actualmente no tienes los permisos necesarios para acceder a esta funcionalidad.',
+					type: 'error',
+				})
+			})
+		}
+}
 
 const PermissionsProvider: React.FC = (props) => {
 	// HISTORY
@@ -12,26 +36,9 @@ const PermissionsProvider: React.FC = (props) => {
 	const company = useContext(BusinessContext)
 	const permissions = company?.business?.permissions
 
+	// LISTENER DE RUTAS
 	useEffect(() => {
-		if (permissions) {
-			// LISTENER
-			router.events.on('routeChangeStart', (location) => {
-				const path: string = location.pathname.substr(1)
-				if (
-					((path.match(/f\/.+$/) || path.match(/f\/.+\/respuestas$/)) && !permissions.forms) ||
-					(path === ROUTES.calendar && !permissions.calendar) ||
-					((path === ROUTES.products || path.startsWith('p/')) && !permissions.products) ||
-					((path === ROUTES.tracking || path.match(/t\/.+\/editar$/)) && !permissions.tracking)
-				) {
-					router.back()
-					window.Alert({
-						title: 'Ocurrió un error',
-						body: 'Actualmente no tienes los permisos necesarios para acceder a esta funcionalidad.',
-						type: 'error',
-					})
-				}
-			})
-		}
+		lockFunction(router.asPath, async () => router.replace(ROUTES.forms), permissions)
 	}, [permissions, router])
 
 	return <>{props.children}</>
